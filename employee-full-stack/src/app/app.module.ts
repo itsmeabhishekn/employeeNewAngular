38a6953bc6d9e4e601ff65fc9ca778d8d3ee979d
itsmeabhishekn <abhishekn4454@gmail.com>
2022-12-07 08:19:21 +0530

    added files

---
+++ employee-full-stack/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { NavComponent } from './nav/nav.component';
+import { ViewComponent } from './view/view.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+const myroute: Routes = [
+  {
+    path: "",
+    component: ViewComponent
+  },
+  {
+    path: "add",
+    component: AddComponent
+  },
+  {path:"view",
+component:ViewComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddComponent,
+    NavComponent,
+    ViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(myroute),
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
